c295182be07695d049fdc5ba9ec0f8aeb1fb8708
Hu Zheng <huzheng_001@hotmail.com>
2017-12-26 11:59:18 +0800

    initial files

---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+/*
+ * This file is part of split-gtk.
+ *
+ * split-gtk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * split-gtk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with split-gtk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+
+static GtkWidget *main_window;
+
+
+
+
+static gboolean on_delete_event(GtkWidget * window, GdkEvent *event , gpointer data)
+{
+	gtk_main_quit();
+	return FALSE;
+}
+
+static void create_window()
+{
+	main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position (GTK_WINDOW (main_window), GTK_WIN_POS_CENTER);
+	gtk_window_set_title(GTK_WINDOW (main_window), "split-gtk");
+	gtk_window_set_default_size(GTK_WINDOW (main_window), 410, 470);
+	gtk_container_set_border_width (GTK_CONTAINER (main_window), 5);
+	g_signal_connect (G_OBJECT (main_window), "delete_event", G_CALLBACK (on_delete_event), NULL);
+
+	gtk_container_add(GTK_CONTAINER(main_window), notebook);
+
+	gtk_widget_show_all(main_window);
+}
+
+int main(int argc,char **argv)
+{
+	gtk_init(&argc, &argv);
+	create_window();
+	gtk_main();
+	return 0;
+}
